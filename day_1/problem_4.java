691a3f80492549957de8f6b038fbd0f584a63974
RiswinD <riswinfdo@gmail.com>
2026-03-31 16:04:15 +0530

    new problems

---
+++ day_1/problem_4.java
@@ -0,0 +1,16 @@
+//Print the sum of numbers from 1 to 10.
+
+
+public class problem_4 {
+    public static void main(String args[])
+    {
+        int sum =0;
+        for (int i=1;i<=10;i++)
+        {
+            sum += i;
+        }
+        System.out.print(sum);
+    }
+    
+    
+}
