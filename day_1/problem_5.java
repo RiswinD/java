71806a22d10e76ad57a00e5f16ce9865cde5da2e
RiswinD <riswinfdo@gmail.com>
2026-04-08 09:08:08 +0530

    level 1 problem is completed

---
+++ day_1/problem_5.java
@@ -0,0 +1,12 @@
+//Print the multiplication table of 5 (from 5×1 to 5×10).
+public class problem_5 {
+    public static void main(String ar[])
+    {
+        for (int i=1;i<=10;i++)
+        {
+            System.out.println("5 X "+i+" = "+i*5 );
+        }
+
+    }
+    
+}
