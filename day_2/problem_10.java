5f5582a468e355d3992f72fbe682fe200ab0733a
RiswinD <riswinfdo@gmail.com>
2026-04-08 09:55:26 +0530

    sum of odd digit from 1 to 100

---
+++ day_2/problem_10.java
@@ -0,0 +1,15 @@
+//Print the sum of all odd numbers from 1 to 100.
+
+public class problem_10 {
+    public static void main (String args[])
+    {
+        int sum =0;
+        for (int i =1;i<=100;i+=2)
+        {
+            sum += i;
+            
+        }
+        System.out.println(sum);
+    }
+    
+}
