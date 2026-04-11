36cf71dc2727c4165a81889f8052cad2ed228d9c
RiswinD <riswinfdo@gmail.com>
2026-04-11 15:01:15 +0530

    top of the diamond

---
+++ day_3/problem_18.java
@@ -0,0 +1,27 @@
+/*
+    *
+   ***
+  *****
+ *******
+*********
+ */
+public class problem_18 {
+    public static void main (String arg[])
+    {
+        for (int i =1;i<=5;i++)
+        {
+            for (int j=1;j<=5-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k=1;k<=(2*i)-1;k++)
+            {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+            
+        }
+
+    }
+}
