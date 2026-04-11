db8a1ed9f8f72e7b49ad38e47b684e83ebfb4853
RiswinD <riswinfdo@gmail.com>
2026-04-11 09:47:50 +0530

    star pattern and number pattern

---
+++ day_3/problem_11.java
@@ -0,0 +1,24 @@
+/*
+*
+**
+***
+****
+*****
+ */
+import java.util.*;
+public class problem_11 {
+    public static void main (String arg[])
+    {
+        int n =5;
+        for (int i =1;i<=n;i++)
+        {
+            for (int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+       
+    }
+    
+}
