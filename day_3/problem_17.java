4733cacc58c1c85cd6709eba8db1e2a902253264
RiswinD <riswinfdo@gmail.com>
2026-04-11 14:38:17 +0530

    *****  ****   ***    **     *

---
+++ day_3/problem_17.java
@@ -0,0 +1,26 @@
+/*
+*****
+ ****
+  ***
+   **
+    *
+ */
+public class problem_17 {
+    public static void main (String arg[])
+    {
+        int n=5;
+        for (int i=1;i<=n;i++)
+        {
+            for (int j=1;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k=i;k<=n;k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
