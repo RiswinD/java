12a668fc945096a75a466b6f09f4d81b0433c4f6
RiswinD <riswinfdo@gmail.com>
2026-04-12 07:30:52 +0530

    reverse pyramid

---
+++ day_4/problem_19.java
@@ -0,0 +1,27 @@
+/*
+*********
+ *******
+  *****
+   ***
+    *
+*/
+
+public class problem_19 {
+    public static void main(String args[])
+    {
+        for (int i=1;i<=5;i++)
+        {
+            for (int j=1;j<=i-1;j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k=1;k<=9-2*(i-1);k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
