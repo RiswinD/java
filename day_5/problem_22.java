de49d1731db89c2c42ff58ae31bdfa547cda0b18
RiswinD <riswinfdo@gmail.com>
2026-04-14 12:04:55 +0530

    hollow pyramid

---
+++ day_5/problem_22.java
@@ -0,0 +1,33 @@
+/*
+    *
+   * *
+  *   *
+ *     *
+* * * * *
+*/
+
+public class problem_22 {
+    public static void main(String arg[])
+    {
+        for (int i =1;i<=5;i++)
+        {
+           for (int j =1;j<=5-i;j++)
+           {
+            System.out.print(" ");
+           }
+           for (int k=1;k<=i;k++)
+           {
+            if(k==1||k==i||i==5)
+            {
+                System.out.print("* ");
+            }
+            else
+            {
+                System.out.print("  ");
+            }
+           }
+           System.out.println();
+        }
+    }
+    
+}
