502f4b0aa5c64a787626ed55c4b1b9588cc0b1fe
RiswinD <riswinfdo@gmail.com>
2026-04-16 11:44:29 +0530

    letter pyramid

---
+++ day_6/problem_26.java
@@ -0,0 +1,23 @@
+/*
+A
+AB
+ABC
+ABCD
+ABCDE
+*/
+
+public class problem_26 {
+    public static void main(String arg[])
+    {
+        for (int i=1;i<=5;i++)
+        {
+            for (int j=1;j<=i;j++)
+            {
+                System.out.print((char)('A'+j-1));
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
